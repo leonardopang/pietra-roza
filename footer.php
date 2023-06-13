c7c4ad591b809212ad1b3a3e2a931206e7a49c9f
Leonardo Pang <leonardopang12@gmail.com>
2023-06-13 14:49:58 -0300

    Ajustes layout footer

--- footer.php
+++ footer.php
@@ -1,5 +1,36 @@
 </main>
 <footer class="footer js-animaion">
+  <div class="container-wrap">
+    <div class="footer-column grid three_grids">
+      <div class="footer-column__item">
+        <div class="logo-container">
+          <img src="" alt="">
+        </div>
+        <div class="text-container">
+          <p>Solicite seu orçamento pelo botão abaixo ou envie um e-mail para contato@email.com.</p>
+        </div>
+        <div class="button-container">
+          <a href="" class="button">Solictar Orçamento</a>
+        </div>
+      </div>
+      <div class="footer-column__item footer-column__item-adress">
+        <a href="" target="_blank">
+          <span>Local</span>
+          <span>São Paulo - SP</span>
+          <span>Uberaba-MG</span>
+        </a>
+      </div>
+      <div class="footer-column__item">
+        <h5>Redes</h5>
+        <div class="social-icon">
+          <a href="" target="_blank"></a>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copy">
+      <p>Copyright © Pietra Roza 2023 . Todos os Direitos Reservados</p>
+    </div>
+  </div>
 </footer>
 <?php wp_footer() ?>
 <script src="<?= get_template_directory_uri() ?>/public/js/app.min.js"></script>
