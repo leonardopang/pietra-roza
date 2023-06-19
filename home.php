9106c3edf8b47b2e4b1d8154b40e20a313088403
Leonardo Pang <leonardopang12@gmail.com>
2023-06-19 19:30:03 -0300

    add style e module hero header

--- home.php
+++ home.php
@@ -1,9 +1,21 @@
 <?php //Template name: Home Page 
 ?>
 <?php get_header() ?>
-<section class="hero-header">
-  <div class="container-wrap">
+<?php
 
+$heroHeader = wp_is_mobile() ? site_url() . '/wp-content/uploads/hero-heder-mobile.jpg' : site_url() . '/wp-content/uploads/hero-heder.jpg';
+
+?>
+<section class="hero-header" style="background-image: url('<?= $heroHeader ?>');">
+  <div class="container-wrap">
+    <div class="hero-header-container">
+      <div class="title-container">
+        <h2>Chamada Banner</h2>
+      </div>
+      <div class="button-container">
+        <a href="" class="button button-yellow">Conheça mais <span class="arrow-button">→</span></a>
+      </div>
+    </div>
   </div>
 </section>
 <?php get_footer() ?>
