015c49915ff854261315b5774411a06ae0ade942
Leonardo Pang <leonardopang12@gmail.com>
2023-06-20 15:07:12 -0300

    modulo projetos home versão desk

--- home.php
+++ home.php
@@ -70,12 +70,42 @@ $heroHeader = wp_is_mobile() ? site_url() . '/wp-content/uploads/hero-heder-mobi
         <span>Nossas obras →</span>
         <h2>Projetos e Mostras de decoração</h2>
       </div>
-      <div class="obras-container__posts"></div>
-      <div class="button-container">
-        <a href="" class="button button-orange">Conheça <span class="arrow-button">→</span></a>
+      <div class="obras-container__posts">
+        <?php if (wp_is_mobile()) : ?>
+          <div class="obras-container__posts-slider"></div>
+        <?php else : ?>
+          <div class="obras-container__posts-grid">
+            <div class="obras-container__posts-grid-item item-1x">
+              <img src="" alt="">
+              <a href="">
+                <div class="content">
+                  <h3>nome da obra</h3>
+                </div>
+              </a>
+            </div>
+            <div class="obras-container__posts-grid-item item-1x">
+              <img src="" alt="">
+              <a href="">
+                <div class="content">
+                  <h3>nome da obra</h3>
+                </div>
+              </a>
+            </div>
+            <div class="obras-container__posts-grid-item item-2x">
+              <img src="" alt="">
+              <a href="">
+                <div class="content">
+                  <h3>nome da obra</h3>
+                </div>
+              </a>
+            </div>
+          </div>
+        <?php endif; ?>
+        <div class="button-container">
+          <a href="" class="button button-orange">Conheça <span class="arrow-button">→</span></a>
+        </div>
       </div>
     </div>
-  </div>
 </section>
 <section class="section-content produtos">
   <div class="container-wrap">
