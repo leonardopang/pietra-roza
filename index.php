8a2f2ec309af222a5741a3775122229691d5a63c
Leonardo Pang <leonardopang12@gmail.com>
2023-06-25 15:54:47 -0300

    Ajuste sub menu desk

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php get_header() ?>
+<?php if (have_posts()): ?>
+  <?php while (have_posts()): ?>
+    <?php the_post(); ?>
+    <?php the_content(); ?>
+  <?php endwhile; endif; ?>
 <?php get_footer() ?>
--- taxonomy-material.php
+++ taxonomy-material.php
@@ -1,3 +1,3 @@
 <?php get_header(); ?>
-
+<?php the_title() ?>
 <?php get_footer(); ?>
